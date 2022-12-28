90cf923aafaed6e8f34e65d46ad0ac23a31cf031
tamildoc777 <121478165+tamildoc777@users.noreply.github.com>
2022-12-28 18:38:01 +0530

    update MainActivity.kt

--- app/src/main/java/com/example/samplecicdapp/MainActivity.kt
+++ app/src/main/java/com/example/samplecicdapp/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("t777")
+                    Greeting("7test7")
                 }
             }
         }
