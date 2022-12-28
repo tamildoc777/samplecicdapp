b5de0da096b073efca24430a533fb56839b70810
tamildoc777 <tamildoc777@gmail.com>
2022-12-28 13:41:08 +0530

    Feat: releasefeature commit-6

--- app/src/main/java/com/example/samplecicdapp/MainActivity.kt
+++ app/src/main/java/com/example/samplecicdapp/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("TestAndroid77")
+                    Greeting("Test7Android77")
                 }
             }
         }
